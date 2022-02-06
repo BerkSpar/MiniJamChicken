a30eb0954d3adc0b06e79e4dbd83df20433daf62
Felipe <felipe@lftecnologia.com.br>
2022-02-06 20:54:01 -0300

    add chick

--- Assets/Scripts/ChickController.cs
+++ Assets/Scripts/ChickController.cs
@@ -4,16 +4,23 @@ using UnityEngine;
 
 public class ChickController : MonoBehaviour
 {
-    private string key;
+    private string key = "";
     private GameController gameController;
-    public GameObject itemText;
     public float minSpawnTime = 3f, maxSpawnTime = 7f;
     public float dieTime = 15f;
     private bool canDie = false;
 
+    public MeshRenderer popup;
+    public GameObject corn;
+    public GameObject worm;
+    public GameObject carrot;
+    public GameObject water;
+
+
     private void Start() {
         gameController = GameObject.FindWithTag("GameController").GetComponent<GameController>();
         StartCoroutine(spawn());
+        SetIcon(key);
     }
 
     private void OnTriggerEnter(Collider other) {
@@ -27,17 +34,41 @@ public class ChickController : MonoBehaviour
 
         foodItemController.DestroyItem();
 
-        itemText.GetComponent<TextMesh>().text = "";
         canDie = false;
+        key = "";
+        SetIcon(key);
 
         StartCoroutine(spawn());
     }
 
-    private void AskForFood() {
-        string[] foodTypes = new string[]{"corn", "worm", "carrot"};
+    private void SetIcon(string key) {
+        worm.SetActive(false);
+        carrot.SetActive(false);
+        water.SetActive(false);
+        corn.SetActive(false);
 
-        key = foodTypes[Random.Range(0, 3)];
-        itemText.GetComponent<TextMesh>().text = key;
+        switch(key) {
+            case "corn": 
+                corn.SetActive(true);
+                break;
+            case "worm": 
+                worm.SetActive(true);
+                break;
+            case "carrot": 
+                carrot.SetActive(true);
+                break;                                
+            case "water": 
+                water.SetActive(true);
+                break;                
+        }
+    }
+
+    private void AskForFood() {
+        string[] foodTypes = new string[]{"corn", "worm", "carrot", "water"};
+
+        key = foodTypes[Random.Range(0, 4)];
+
+        SetIcon(key);
     }
 
     IEnumerator spawn()
